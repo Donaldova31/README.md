cb9440da2465b660e2f891922dbfbb7fe501b35b
Donaldova31 <donaldo_va8@outlook.com>
2025-04-21 16:02:20 -0600

    Update FirstPythonScripts.py

--- FirstPythonScripts.py
+++ FirstPythonScripts.py
@@ -2,4 +2,4 @@
 print("Hey, I'm trying to figure this environmnet out!")
 
 # Now i've added some complement text:
-print("it seems i little easy to solve this platform")
+print("it seems a bit easy to solve this platform")
